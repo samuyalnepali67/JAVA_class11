c402877c461a14abf15e51e63bae5296f276a3cd
samuyalnepali67 <samuyalnepali67@gmail.com>
2025-11-20 01:30:02 -0800

    classwork

---
+++ Day_8/Oddnum.java
@@ -0,0 +1,9 @@
+public class Oddnum{
+        public static void main(String[]args){
+        for(int i=1; i<=20; i++){
+            int odd=2*i-1;
+            System.out.println(odd);
+        }
+
+        }
+}
