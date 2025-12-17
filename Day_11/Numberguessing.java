9778d057fc55172d3f402f6da2bb56e79b9a9190
samuyalnepali67 <samuyalnepali67@gmail.com>
2025-12-17 09:04:16 +0545

    update

--- Day_11/Numberguessing.java
+++ Day_11/Numberguessing.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class  Numberguessing {
    int num;
    int num2;
+ 
 
    Numberguessing(int num){
     this.num = num;
@@ -11,24 +12,34 @@ public class  Numberguessing {
    
    void check(){
        Scanner input = new Scanner(System.in);
-       
+       boolean check = true;
+        int attempt =1;
 
-     while(true){
+     while(check){
         System.out.println("Enter the number");
         num2 = input.nextInt();
 
         if(num2>num){
             System.out.println("Your number " + num2 + " is greater");
+            attempt++;
         }
 
         else if(num2<num){
             System.out.println("Your number " + num2 + " is smaller");
+            attempt++;
         }
       
-      else if(num2 == num){
+        else {
          System.out.println("Your guess is correct ");
+            System.out.println("Your guessed in  " + attempt + " attempt");
+         check = false;
+
+         
         
       }
+
+    
+     
      }
    }
 
